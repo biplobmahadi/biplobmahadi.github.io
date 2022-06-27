1bae34151b87b24df400177eb8612300bd2808e6
Biplob <biplob.ruet15@gmail.com>
2022-06-28 03:02:27 +0600

    update about page

--- src/components/CommonHead.tsx
+++ src/components/CommonHead.tsx
@@ -1,7 +1,7 @@
+import type { NextPage } from "next";
 import Head from "next/head";
-import React from "react";
 
-const CommonHead = () => {
+const CommonHead: NextPage = () => {
   return (
     <Head>
       <title>Welcome</title>
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,5 +1,4 @@
 import type { NextPage } from "next";
-import React from "react";
 
 const About: NextPage = () => {
   return <div>About me</div>;
