d2a45aec99788baeb8ae340a0e08f1b3c456dc6a
Biplob <biplob.ruet15@gmail.com>
2022-06-28 02:52:44 +0600

    add my first blog as testing

---
+++ pages/blogs/advance-axios.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import React from "react";
+
+const AdvanceAxios: NextPage = () => {
+  return <div>AdvanceAxios blog</div>;
+};
+
+export default AdvanceAxios;
