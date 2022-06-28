6ef75d8b69e079437f6e5754ec5c3a43b9c65ad8
Biplob <biplob.ruet15@gmail.com>
2022-06-29 03:01:15 +0600

    change layout

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -17,21 +17,20 @@ import {
   useMantineColorScheme,
 } from "@mantine/core";
 import { useWindowScroll } from "@mantine/hooks";
-import { Sun, MoonStars } from "tabler-icons-react";
+import { Sun, MoonStars, ArrowUp, User, Home } from "tabler-icons-react";
+
 import IChildProp from "../../interfaces/common/IChildProp";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function Layout({ children }: IChildProp) {
   const theme = useMantineTheme();
-  const [opened, setOpened] = useState(false);
   const [scroll, scrollTo] = useWindowScroll();
   const { colorScheme, toggleColorScheme } = useMantineColorScheme();
+  const router = useRouter();
 
   return (
     <Fragment>
-      <Head>
-        <meta name="description" content="My Personal App to Share Knowledge" />
-        <link rel="icon" href="/main.ico" />
-      </Head>
       <AppShell
         styles={{
           main: {
@@ -41,28 +40,29 @@ export default function Layout({ children }: IChildProp) {
                 : theme.colors.gray[0],
           },
         }}
-        navbarOffsetBreakpoint="sm"
-        asideOffsetBreakpoint="sm"
         fixed
         header={
           <Header height={70} p="md">
-            <Group
-              position="apart"
-              // style={{ display: "flex", alignItems: "center", height: "100%" }}
-            >
-              <MediaQuery largerThan="sm" styles={{ display: "none" }}>
-                <Burger
-                  opened={opened}
-                  onClick={() => setOpened((o) => !o)}
-                  size="sm"
-                  color={theme.colors.gray[6]}
-                  mr="xl"
-                />
-              </MediaQuery>
-
-              <Text weight={700} size="lg">
-                Welcome, It&apos;s Biplob
-              </Text>
+            <Group position="apart">
+              <ActionIcon
+                onClick={() => router.push("/")}
+                variant="default"
+                size={30}
+              >
+                <Home size={16} />
+              </ActionIcon>
+              <Group>
+                <Text weight={700} size="lg">
+                  Welcome, It&apos;s Biplob
+                </Text>
+                <ActionIcon
+                  onClick={() => router.push("/about")}
+                  variant="default"
+                  size={30}
+                >
+                  <User size={16} />
+                </ActionIcon>
+              </Group>
               <ActionIcon
                 variant="default"
                 onClick={() => toggleColorScheme()}
@@ -77,25 +77,7 @@ export default function Layout({ children }: IChildProp) {
             </Group>
           </Header>
         }
-        navbar={
-          <Navbar
-            p="md"
-            hiddenBreakpoint="sm"
-            hidden={!opened}
-            width={{ sm: 200, lg: 300 }}
-          >
-            <Text>Application navbar</Text>
-          </Navbar>
-        }
-        aside={
-          <MediaQuery smallerThan="sm" styles={{ display: "none" }}>
-            <Aside p="md" hiddenBreakpoint="sm" width={{ sm: 200, lg: 300 }}>
-              <Text>Application sidebar</Text>
-            </Aside>
-          </MediaQuery>
-        }
       >
-        <Text>Resize app to see responsive navbar in action</Text>
         {children}
       </AppShell>
 
@@ -103,7 +85,7 @@ export default function Layout({ children }: IChildProp) {
         <Transition transition="slide-up" mounted={scroll.y > 0}>
           {(transitionStyles) => (
             <Button style={transitionStyles} onClick={() => scrollTo({ y: 0 })}>
-              Scroll to top
+              <ArrowUp />
             </Button>
           )}
         </Transition>
