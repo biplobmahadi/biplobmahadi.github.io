92e13a54ee895b31508695acf575515239dff105
Biplob <biplob.ruet15@gmail.com>
2022-06-29 01:54:57 +0600

    modify app file to fit layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,38 @@
 import type { AppProps } from "next/app";
 import { Fragment } from "react";
-import CommonHead from "../components/CommonHead";
+import {
+  ColorScheme,
+  ColorSchemeProvider,
+  MantineProvider,
+} from "@mantine/core";
+import { useLocalStorage } from "@mantine/hooks";
+import Layout from "../components/common/Layout";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
+    key: "mantine-color-scheme",
+    defaultValue: "light",
+    getInitialValueInEffect: true,
+  });
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
+
   return (
     <Fragment>
-      <CommonHead />
-      <Component {...pageProps} />
+      <ColorSchemeProvider
+        colorScheme={colorScheme}
+        toggleColorScheme={toggleColorScheme}
+      >
+        <MantineProvider
+          theme={{ colorScheme }}
+          withGlobalStyles
+          withNormalizeCSS
+        >
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </MantineProvider>
+      </ColorSchemeProvider>
     </Fragment>
   );
 }
