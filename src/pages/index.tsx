d715b1a9599c212638b502c69a93d6f0cf08c1c4
Biplob <biplob.ruet15@gmail.com>
2022-06-29 01:55:13 +0600

    update home page for demo purpose

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,46 @@
 import type { NextPage } from "next";
+import { Prism } from "@mantine/prism";
+import { useMantineTheme } from "@mantine/core";
+
+const demoCode = `import { Button } from '@mantine/core';
+
+function Demo() {
+  return <Button>Hello</Button>
+}`;
 
 const Home: NextPage = () => {
+  const theme = useMantineTheme();
   return (
     <div>
-      <main>Hello Man!</main>
+      <main style={{ marginTop: "100px" }}>Hello Man!</main>
+      <main style={{ marginTop: "100px" }}>Hello Man!</main>
+      <main style={{ marginTop: "100px" }}>Hello Man!</main>
+      <main style={{ marginTop: "100px" }}>Hello Man!</main>
+      <main style={{ marginTop: "100px" }}>Hello Man!</main>
+      <main style={{ marginTop: "100px" }}>Hello Man!</main>
+      <main style={{ marginTop: "100px" }}>Hello Man!</main>
+      <main style={{ marginTop: "100px" }}>Hello Man!</main>
+      <main style={{ marginTop: "100px" }}>Hello Man!</main>
+      <main style={{ marginTop: "100px" }}>Hello Man!</main>
+      <Prism
+        sx={{ marginTop: "100px" }}
+        language="tsx"
+        withLineNumbers
+        colorScheme={theme.colorScheme === "dark" ? "light" : "dark"}
+      >
+        {demoCode}
+      </Prism>
+      <Prism
+        sx={{ marginTop: "100px" }}
+        language="tsx"
+        withLineNumbers
+        colorScheme={theme.colorScheme === "dark" ? "light" : "dark"}
+      >
+        {demoCode}
+      </Prism>
+
+      <main style={{ marginTop: "100px" }}>Hello Man!</main>
+      <main style={{ marginTop: "100px" }}>Hello Man!</main>
     </div>
   );
 };
