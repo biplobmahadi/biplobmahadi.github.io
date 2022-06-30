8d0791ed2f8d5bdbc46781ef83ff1dd5d1c352a2
Biplob <biplob.ruet15@gmail.com>
2022-07-01 02:11:22 +0600

    create app layout where only docs path will show navbar and aside

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -1,95 +1,50 @@
-import { Fragment, useState } from "react";
-import Head from "next/head";
+import React, { useState } from "react";
 import {
   AppShell,
-  Navbar,
-  Header,
   Aside,
   Text,
   MediaQuery,
-  Burger,
   useMantineTheme,
-  Affix,
-  Button,
-  Transition,
-  Group,
-  ActionIcon,
-  useMantineColorScheme,
 } from "@mantine/core";
-import { useWindowScroll } from "@mantine/hooks";
-import { Sun, MoonStars, ArrowUp, User, Home } from "tabler-icons-react";
-
+import CustomHeader from "./Header";
 import IChildProp from "../../interfaces/common/IChildProp";
-import Link from "next/link";
+import CustomNavbar from "./navbar/Navbar";
 import { useRouter } from "next/router";
 
 export default function Layout({ children }: IChildProp) {
   const theme = useMantineTheme();
-  const [scroll, scrollTo] = useWindowScroll();
-  const { colorScheme, toggleColorScheme } = useMantineColorScheme();
-  const router = useRouter();
+  const [opened, setOpened] = useState(false);
+  const { pathname } = useRouter();
+  const folderPath = pathname.substring(1, pathname.lastIndexOf("/"));
 
   return (
-    <Fragment>
-      <AppShell
-        styles={{
-          main: {
-            background:
-              theme.colorScheme === "dark"
-                ? theme.colors.dark[8]
-                : theme.colors.gray[0],
-          },
-        }}
-        fixed
-        header={
-          <Header height={70} p="md">
-            <Group position="apart">
-              <ActionIcon
-                onClick={() => router.push("/")}
-                variant="default"
-                size={30}
-              >
-                <Home size={16} />
-              </ActionIcon>
-              <Group>
-                <Text weight={700} size="lg">
-                  Welcome, It&apos;s Biplob
-                </Text>
-                <ActionIcon
-                  onClick={() => router.push("/about")}
-                  variant="default"
-                  size={30}
-                >
-                  <User size={16} />
-                </ActionIcon>
-              </Group>
-              <ActionIcon
-                variant="default"
-                onClick={() => toggleColorScheme()}
-                size={30}
-              >
-                {colorScheme === "dark" ? (
-                  <Sun size={16} />
-                ) : (
-                  <MoonStars size={16} />
-                )}
-              </ActionIcon>
-            </Group>
-          </Header>
-        }
-      >
-        {children}
-      </AppShell>
-
-      <Affix position={{ bottom: 20, right: 20 }}>
-        <Transition transition="slide-up" mounted={scroll.y > 0}>
-          {(transitionStyles) => (
-            <Button style={transitionStyles} onClick={() => scrollTo({ y: 0 })}>
-              <ArrowUp />
-            </Button>
-          )}
-        </Transition>
-      </Affix>
-    </Fragment>
+    <AppShell
+      styles={{
+        main: {
+          background:
+            theme.colorScheme === "dark"
+              ? theme.colors.dark[8]
+              : theme.colors.gray[0],
+        },
+      }}
+      navbarOffsetBreakpoint="sm"
+      asideOffsetBreakpoint="sm"
+      fixed
+      header={<CustomHeader opened={opened} setOpened={setOpened} />}
+      navbar={
+        folderPath === "docs" ? <CustomNavbar opened={opened} /> : undefined
+      }
+      aside={
+        folderPath === "docs" ? (
+          <MediaQuery smallerThan="sm" styles={{ display: "none" }}>
+            <Aside p="md" hiddenBreakpoint="sm" width={{ sm: 200, lg: 260 }}>
+              <Text>Application sidebar</Text>
+            </Aside>
+          </MediaQuery>
+        ) : undefined
+      }
+    >
+      {children}
+    </AppShell>
   );
 }
