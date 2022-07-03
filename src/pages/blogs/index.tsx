b17dd27d890119007e3ae29dd8537ac88865100f
Biplob <biplob.ruet15@gmail.com>
2022-07-04 01:07:29 +0600

    fix link path for blogs in home page

--- src/pages/blogs/index.tsx
+++ src/pages/blogs/index.tsx
@@ -1,15 +1,22 @@
+import Head from "next/head";
 import Link from "next/link";
 import { Fragment } from "react";
 import { blogTitlesAndPaths } from "../../../blogs";
 import { IBlogTitleAndPaths } from "../../interfaces/blogs";
+import UIPaths from "../../paths/uiPaths";
 
 const WelcomeToBlog = () => {
   return (
     <Fragment>
+      <Head>
+        <title>Welcome to my blog</title>
+      </Head>
       <h4>Welcome to my blog</h4>
       {blogTitlesAndPaths.map((blog: IBlogTitleAndPaths) => (
         <div key={blog.title}>
-          <Link href={`/${blog.path}`}>{blog.title}</Link>
+          <Link href={UIPaths.Blogs.GetSingleBlog(blog.path)}>
+            {blog.title}
+          </Link>
         </div>
       ))}
     </Fragment>
