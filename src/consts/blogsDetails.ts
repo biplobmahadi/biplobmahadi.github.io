64767d222f6cb80d18fae654b51984834b0ec3f0
Biplob <biplob.ruet15@gmail.com>
2022-07-08 16:06:41 +0600

    complete demo blogs system

--- src/consts/blogsDetails.ts
+++ src/consts/blogsDetails.ts
@@ -1,6 +1,6 @@
 import { IBlogsDetails } from "../interfaces/blogs";
 
 export const blogsDetails: IBlogsDetails[] = [
-  { title: "Hello World", path: "hello-world" },
+  { title: "Hello World 1", path: "hello-world-1" },
   { title: "Hello World 2", path: "hello-world-2" },
 ];
