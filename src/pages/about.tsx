0575a580286eef8955509350bf69356c348175b6
Biplob <biplob.ruet15@gmail.com>
2022-07-11 16:59:38 +0600

    add head

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,7 +1,16 @@
 import type { NextPage } from "next";
+import Head from "next/head";
+import { Fragment } from "react";
 
 const About: NextPage = () => {
-  return <div>About me</div>;
+  return (
+    <Fragment>
+      <Head>
+        <title>About</title>
+      </Head>
+      <div>About me</div>
+    </Fragment>
+  );
 };
 
 export default About;
