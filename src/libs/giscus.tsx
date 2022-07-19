a9c89d4d5d4071b1a6d8ccb01245268d6deea3a5
Biplob <biplob.ruet15@gmail.com>
2022-07-19 23:52:10 +0600

    update giscus with env and add giscus config json

--- src/libs/giscus.tsx
+++ src/libs/giscus.tsx
@@ -3,13 +3,18 @@ import Giscus from "@giscus/react";
 import { IGiscusDiscussion } from "../interfaces/libs/giscus.interface";
 
 const GiscusDiscussion: FC<IGiscusDiscussion> = ({ theme }) => {
+  const githubUsername = process.env.NEXT_PUBLIC_GITHUB_USERNAME;
+  const githubRepoName = process.env.NEXT_PUBLIC_GITHUB_REPO_NAME;
+  const githubRepoId = process.env.NEXT_PUBLIC_GITHUB_REPO_ID;
+  const githubCategoryId = process.env.NEXT_PUBLIC_GITHUB_CATEGORY_ID;
+
   return (
     <Giscus
       id="comments"
-      repo="biplobmahadi/biplobmahadi.github.io"
-      repoId="R_kgDOHkgVOA"
+      repo={`${githubUsername}/${githubRepoName}`}
+      repoId={githubRepoId!}
       category="Comments"
-      categoryId="DIC_kwDOHkgVOM4CQGH7"
+      categoryId={githubCategoryId}
       mapping="url"
       term="Welcome!"
       reactionsEnabled="1"
