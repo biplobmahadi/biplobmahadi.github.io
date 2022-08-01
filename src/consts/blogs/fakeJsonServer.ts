363e47b2b55092cf2e745fd6172aabb7493e89c5
Biplob <biplob.ruet15@gmail.com>
2022-08-02 00:39:36 +0600

    update prism code for fake json server

--- src/consts/blogs/fakeJsonServer.ts
+++ src/consts/blogs/fakeJsonServer.ts
@@ -1,29 +1,29 @@
 export const responseFormat = `{ isSuccess: boolean; data: actual data; message: string; }`;
 
 export const dbCode = `{
-    "posts": [
-      { "id": 1, "title": "json-server", "author": "typicode" },
-      { "id": 2, "title": "json", "author": "react" },
-      { "id": 3, "title": "server", "author": "typescript" },
-      { "id": 4, "title": "demo-server", "author": "redux" }
-      { "id": 5, "title": "server-demo", "author": "axios" }
-    ],
+  "posts": [
+    { "id": 1, "title": "json-server", "author": "typicode" },
+    { "id": 2, "title": "json", "author": "react" },
+    { "id": 3, "title": "server", "author": "typescript" },
+    { "id": 4, "title": "demo-server", "author": "redux" },
+    { "id": 5, "title": "server-demo", "author": "axios" }
+  ]
 }`;
 
 export const packageJson = `{
-    "name": "backend",
-    "version": "1.0.0",
-    "description": "",
-    "main": "index.js",
-    "scripts": {
-        "start": "node server.js"
-    },
-    "keywords": [],
-    "author": "",
-    "license": "ISC",
-    "dependencies": {
-      "json-server": "^0.17.0"
-    }
+  "name": "backend",
+  "version": "1.0.0",
+  "description": "",
+  "main": "index.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "keywords": [],
+  "author": "",
+  "license": "ISC",
+  "dependencies": {
+    "json-server": "^0.17.0"
+  }
 }`;
 
 export const serverCode = `const path = require("path");
@@ -36,11 +36,13 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use(router);
 
-router.render = (req, res) => {
+router.render = (_req, res) => {
+  const { statusCode, locals: { data } } = res;
+
   res.jsonp({
-    isSuccess: res.statusCode !== 404 ? true : false,
-    data: res.statusCode !== 404 ? res.locals.data : null,
-    message: res.statusCode !== 404 ? "Successfully done!" : "Error occurred!",
+    isSuccess: (statusCode >= 200 && statusCode < 300) ? true : false,
+    data: (statusCode >= 200 && statusCode < 300) ? data : null,
+    message: (statusCode >= 200 && statusCode < 300) ? "Successfully done!" : "Error occurred!",
   });
 };
 
